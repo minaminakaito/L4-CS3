79e1a407a1613d37280612ec1245ae5bedc65770
Ubuntu <ubuntu@ip-172-31-44-157.ec2.internal>
2022-10-11 07:37:16 +0000

    ●●●

---
+++ a.rb
@@ -0,0 +1,9 @@
+require 'bcrypt'
+
+signup_password = BCrypt::Password.create("my password")
+
+login_password = BCrypt::Password.new(signup_password)
+
+if login_password == "my password"
+    p "ログイン成功"
+end
---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,23 @@
+class TopController < ApplicationController
+    def main
+        if session[:login_uid]
+            render "main"
+        else
+            render "login"
+        end
+    end
+    def login
+        user = User.find_by(uid: params[:uid])
+        login_password = BCrypt::Password.new(user.pass)
+        if login_password == params[:pass]
+            session[:login_uid] = params[:uid]
+            redirect_to root_path
+        else
+            render 'error'
+        end
+    end
+    def logout
+        session.delete(:login_uid)
+        render "login"
+    end
+end
